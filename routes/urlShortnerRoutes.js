7a057499d567c27d25ab61002a84a85afeb4f394
bhavani shanker <143577924+bhavanishanker-png@users.noreply.github.com>
2024-12-06 19:16:22 +0530

    Update urlShortnerRoutes.js

--- routes/urlShortnerRoutes.js
+++ routes/urlShortnerRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const { shortenUrl, getShortenUrl } = require('../controllers/urlShortnerController');
+const { shortenUrl, getShortenUrl, redirectToLongUrl } = require('../controllers/urlShortnerController');
 
 const router = express.Router();
 
+// Route to shorten a URL
 router.post('/url', shortenUrl);
-router.get('/url', getShortenUrl)
-router.get("/", (req, res) => {
-    res.send("Hello, world!");
+// Route to get the shortened URL for a given long URL
+router.get('/url', getShortenUrl);
+// Route to handle redirection from short URL to long URL
+router.get('/:shortUrl', redirectToLongUrl);
+
+// Default route for testing or debugging
+router.get('/', (req, res) => {
+    res.send("Welcome to the URL Shortener API!");
 });
+
 module.exports = router;
